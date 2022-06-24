875ef2b36c396b152be58c90552e185234cfa83a
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2022-06-24 14:37:13 +0630

    update design

---
+++ src/components/Scary.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="grid grid-cols-2">
+    <div>NFTs Are not The Future Cannot Be Scary</div>
+    <div></div>
+  </div>
+</template>
--- src/components/card/Card.vue
+++ src/components/card/Card.vue
@@ -17,29 +17,32 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="relative p-4 w-full bg-white rounded-lg overflow-hidden shadow"
-    style="min-height: 160px"
-  >
-    <div>
-      <div class="relative block h-full">
-        <img
-          class="rounded-lg h-32 w-full object-cover"
-          :src="`src/assets/images/${image}.jpg`"
-        />
+  <div class="relative">
+    <div class="bg-red-500 w-full h-full absolute blur"></div>
+    <div
+      class="relative p-4 w-full bg-transparent rounded-lg overflow-hidden shadow"
+      style="min-height: 160px"
+    >
+      <div>
+        <div class="relative block h-full">
+          <img
+            class="rounded-lg h-32 w-full object-cover"
+            :src="`src/assets/images/${image}.jpg`"
+          />
+        </div>
       </div>
-    </div>
-    <p class="mt-2 text-gray-800 text-sm">{{ status }}</p>
-    <div class="flex justify-between">
-      <h2 class="mt-2 text-gray-800 text-sm font-semibold line-clamp-1">
-        {{ price }}
-      </h2>
+      <p class="mt-2 text-gray-800 text-sm">{{ status }}</p>
+      <div class="flex justify-between">
+        <h2 class="mt-2 text-gray-800 text-sm font-semibold line-clamp-1">
+          {{ price }}
+        </h2>
 
-      <button
-        class="px-4 py-2 bg-gradient-to-r from-red-500 to-pink-600 text-white text-sm font-medium rounded-md"
-      >
-        Bid Now
-      </button>
+        <button
+          class="px-4 py-2 bg-gradient-to-r from-red-500 to-pink-600 text-white text-sm font-medium rounded-md"
+        >
+          Bid Now
+        </button>
+      </div>
     </div>
   </div>
 </template>
