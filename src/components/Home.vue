5c3dfe4d4d696ddffe459937e05427bed467879b
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2022-06-26 22:22:49 +0630

    Update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,8 @@
 import Button from "@/components/Button/Button.vue";
 </script>
 <template>
-  <div class="grid grid-cols-1 xl:grid-cols-2 mt-20">
-    <div class="space-y-6">
+  <div class="grid grid-cols-1 xl:grid-cols-2 space-x-14">
+    <div class="space-y-6 mt-20">
       <div class="text-zinc-400 text-sm font-semibold">NFT MARKET PLACE!</div>
       <div class="text-white text-5xl leading-snug">
         The <span class="underline font-bold">Most</span> <br />
@@ -36,32 +36,45 @@ import Button from "@/components/Button/Button.vue";
       </div>
       <div class="flex h-10">
         <Button :name="'Explore Now'" :is_button_half_rounded="true" />
-        <div class="flex">
+        <div class="flex ml-5 mt-2">
           <svg
-            class="w-6 h-6 fill-transparent"
-            stroke="bg-gradient-to-r from-red-500 to-pink-600"
+            class="w-6 h-6 text-red-500"
+            fill="none"
+            stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
+              class="text-white"
+              fill="none"
+              stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
               d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
             ></path>
             <path
+              class="bg-red"
+              fill="none"
+              stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
               d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             ></path>
           </svg>
-          <span class="text-white text-sm font-bold">Demo Explore Now</span>
+          <span class="text-zinc-400 text-sm font-bold ml-3"
+            >Demo Explore Now</span
+          >
         </div>
       </div>
     </div>
 
-    <div class="block">
+    <div class="flex mx-auto mt-16">
+      <img
+        class="rounded-lg h-96 w-full object-cover"
+        src="src/assets/images/four.jpg"
+      />
     </div>
   </div>
 </template>
