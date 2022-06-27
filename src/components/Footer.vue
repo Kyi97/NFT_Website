268da54cfc723ee3c0eb4467739b08276d51a416
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2022-06-27 10:34:21 +0630

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,33 @@
     class="grid xs:grid-cols-1 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-11 text-sm text-gray-400 mt-44 bg-black px-7 md:px-10 lg:px-24 py-14"
   >
     <div class="sm:col-span-2 lg:col-span-3 pr-7 space-y-3 mb-14">
-      <div class="text-lg text-white">(icon) DIGITL AGENCY</div>
+      <div class="flex text-lg text-white">
+        <img
+          src="src/assets/images/wk.jpg"
+          alt="Logo"
+          class="h-7 w-8 mr-2 rounded-lg"
+        />
+        DIGITL AGENCY
+      </div>
       <div>
         lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
         quidem.
       </div>
       <div>Follow Us ---></div>
-      <div>(icon) (icon) (icon)</div>
+      <div class="flex space-x-2">
+        <img
+          src="https://img.icons8.com/ios-glyphs/30/000000/facebook-new.png"
+          class="bg-white rounded-md w-5 h-5 hover:scale-110 ease-in duration-100 transition cursor-pointer"
+        />
+        <img
+          src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png"
+          class="bg-white rounded-md w-5 h-5 hover:scale-110 ease-in duration-100 transition cursor-pointer"
+        />
+        <img
+          src="https://img.icons8.com/material-outlined/24/000000/instagram-new--v1.png"
+          class="bg-white rounded-md w-5 h-5 hover:scale-110 ease-in duration-100 transition cursor-pointer"
+        />
+      </div>
     </div>
     <div class="lg:col-span-2 pr-7 space-y-2 mb-14">
       <div class="text-lg text-white">Explore</div>
