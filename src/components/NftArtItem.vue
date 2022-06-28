75d8853c2cdd7943f5d9c860d3b303d5536971f5
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2022-06-28 11:20:27 +0630

    update

--- src/components/NftArtItem.vue
+++ src/components/NftArtItem.vue
@@ -8,61 +8,72 @@ let items = ref([
     price: "1.26 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "one",
+    image: "./src/assets/images/one.jpg",
   },
   {
     id: 1,
     price: "1.27 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "two",
+    image: "./src/assets/images/one.jpg",
   },
   {
     id: 2,
     price: "1.28 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "three",
+    image: "./src/assets/images/two.jpg",
   },
   {
     id: 3,
     price: "1.29 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "four",
+    image: "./src/assets/images/three.jpg",
   },
   {
     id: 4,
     price: "1.26 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "five",
+    image: "./src/assets/images/four.jpg",
   },
   {
     id: 5,
     price: "1.27 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "six",
+    image: "./src/assets/images/five.jpg",
   },
   {
     id: 6,
     price: "1.28 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "seven",
+    image: "./src/assets/images/six.jpg",
   },
   {
     id: 7,
     price: "1.29 ETH",
     sub_price: "$15253.13",
     status: "Current Price",
-    image: "eight",
+    image: "./src/assets/images/seven.jpg",
   },
 ]);
 </script>
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 w-full mt-10">
+  <div
+    class="
+      grid grid-cols-1
+      md:grid-cols-2
+      lg:grid-cols-3
+      xl:grid-cols-3
+      2xl:grid-cols-4
+      gap-4
+      w-full
+      mt-10
+    "
+  >
     <Card
       v-for="item in items"
       :key="item.id"
